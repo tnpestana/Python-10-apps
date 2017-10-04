d3140b4c7350591b6c1ba2d96faeaf7dd2c92650
Tiago Pestana <tnevespestana@gmail.com>
2017-10-04 18:23:16 +0100

    coding exercise 1

---
+++ 4-section/1-coding-exercise/1exercise.py
@@ -0,0 +1,7 @@
+def convert_celsius(celsius):
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    return fahrenheit
+
+celsius_input = input("enter celsius degrees: ")
+print("value in fahrenheit: " + str(convert_celsius(celsius_input)))
+print("thank you")
