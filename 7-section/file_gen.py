d870d3e989ce4df5984f5f0beaa5052a17f30a78
Tiago Pestana <tnevespestana@gmail.com>
2017-10-06 17:29:03 +0100

    comment previous code

--- 7-section/file_gen.py
+++ 7-section/file_gen.py
@@ -1,8 +1,10 @@
 import datetime
 
+# store current date and time 
 filename=datetime.datetime.now()
 
 def create_file():
+    # strftime() method provides a string format for the datetime object
     with open("7-section/time_files/"+filename.strftime("%Y-%m-%d--%H-%M")+".txt", "w") as file:
         file.write("")
 
