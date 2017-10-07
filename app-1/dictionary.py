0b522d543f53e5fadb08ee8adf54107a4600031b
Tiago Pestana <tnevespestana@gmail.com>
2017-10-07 20:24:18 +0100

    optimized output

--- app-1/dictionary.py
+++ app-1/dictionary.py
@@ -5,19 +5,23 @@ from difflib import get_close_matches
 data=json.load(open("app-1/data.json"))
 
 def main():
-    # prompt the user fo=r  a word to search
+    # prompt the user for a word to search
     key=input("\nword to search: ")
     print(find_word(key))
 
 def find_word(word):
     word=word.lower()
     if word in data:
-        return data[word]
+        for x in range(0, len(data[word])):
+            print("> %s" % data[word][x])
+        return "\nthank you"
     elif len(get_close_matches(word, data.keys())) > 0:
         print("\ndid you mean %s instead?" % get_close_matches(word, data.keys())[0])
         answer=input("type Y for yes or N for no: ").lower()
         if answer=="y":
-            return data[get_close_matches(word, data.keys())[0]]
+            for x in range(0, len(data[get_close_matches(word, data.keys())[0]])):
+                print("> %s" % data[get_close_matches(word, data.keys())[0]][x])
+            return "\nthank you"
         elif answer=="n":
             main()
         else:
@@ -26,5 +30,5 @@ def find_word(word):
         return "sorry, couldn't find the word"
 
 # program begins
-print("\npestos english dictionary")
+print("\n*pestos english dictionary*")
 main()
