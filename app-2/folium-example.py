3704026649f03b72300f463e874fd4b40381653d
Tiago Pestana <tnevespestana@gmail.com>
2017-10-11 19:36:59 +0100

    folium markers and feature groups

--- app-2/folium-example.py
+++ app-2/folium-example.py
@@ -1,3 +1,11 @@
 import folium
-map = folium.Map(location=[80,-100])
+
+# create a map object (removed tiles="Mapbox Bright")
+map = folium.Map(location=[38.76, -9.2], zoom_start=10)
+# create a feature group
+group=folium.FeatureGroup(name="my map")
+# create a marker on the map
+group.add_child(folium.Marker(location=[38.76, -9.2], popup="gucci gang gucci gang", icon=folium.Icon(color="green")))
+map.add_child(group)
+
 map.save("map-test.html")
