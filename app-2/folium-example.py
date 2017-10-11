c7450cea1f10e67b5a8630d9fd03bb7742f0df01
Tiago Pestana <tnevespestana@gmail.com>
2017-10-11 20:06:53 +0100

    adding markers from txt file

--- app-2/folium-example.py
+++ app-2/folium-example.py
@@ -1,14 +1,25 @@
 import folium
+import pandas
 
 # create a map object (removed tiles="Mapbox Bright")
-map = folium.Map(location=[38.76, -9.2], zoom_start=10)
+map=folium.Map(location=[38.76, -9.2], zoom_start=10)
 # create a feature group
 group=folium.FeatureGroup(name="my map")
-# iterate through a list of locations (wich are also lists of coordinates) to create multiple markers
-for coordinates in [[38.76, -9.2], [38.75, -9.21]]:
-    # create a marker on the map
-    group.add_child(folium.Marker(location=coordinates, popup="gucci gang gucci gang", icon=folium.Icon(color="green")))
 
+# store data from a txt file in python
+data=pandas.read_csv("Volcanoes_USA.txt")
+# separate the latitudes and longitudes into individual pyhton lists
+latitude=list(data["LAT"])
+longitude=list(data["LON"])
+
+# iterate through a list of latitudes and longitudes to create multiple markers
+# the zip object returns a tuple with two values from the lists and its next() method increments them at the same time
+for lat, lon in zip(latitude, longitude):
+    # create a marker on the map
+    group.add_child(folium.Marker(location=[lat,lon], popup="gucci gang gucci gang", icon=folium.Icon(color="green")))
+
+# add feature group properties to our map
 map.add_child(group)
 
+# write html file
 map.save("map-test.html")
