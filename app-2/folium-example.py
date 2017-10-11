9190d90755fa40aae6cfafa82400795e91107d63
Tiago Pestana <tnevespestana@gmail.com>
2017-10-11 19:46:35 +0100

    adding multiple markers

--- app-2/folium-example.py
+++ app-2/folium-example.py
@@ -4,8 +4,11 @@ import folium
 map = folium.Map(location=[38.76, -9.2], zoom_start=10)
 # create a feature group
 group=folium.FeatureGroup(name="my map")
-# create a marker on the map
-group.add_child(folium.Marker(location=[38.76, -9.2], popup="gucci gang gucci gang", icon=folium.Icon(color="green")))
+# iterate through a list of locations (wich are also lists of coordinates) to create multiple markers
+for coordinates in [[38.76, -9.2], [38.75, -9.21]]:
+    # create a marker on the map
+    group.add_child(folium.Marker(location=coordinates, popup="gucci gang gucci gang", icon=folium.Icon(color="green")))
+
 map.add_child(group)
 
 map.save("map-test.html")
