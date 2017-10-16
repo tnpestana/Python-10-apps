d9b56b1e5943855dc1a1b8bcd756b1fa4173f562
Tiago Pestana <tnevespestana@gmail.com>
2017-10-16 18:59:59 +0100

    write website list into hosts_temp file

--- app-3/website-blocker.py
+++ app-3/website-blocker.py
@@ -2,14 +2,22 @@ import time
 from datetime import datetime as dt
 
 # store data for future use
-hots_path=r"C:\Windows\System32\drivers\etc\hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+hosts_temp="hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com", "www.instagram.com", "www.tumblr.com", "www.twitter.com"]
+website_list=["www.facebook.com", "facebook.com", "www.instagram.com", "www.tumblr.com"]
 
 # while loop makes the program keep running until further notice
 while True:
-    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 16):
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 21):
         print("working hours")
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
     else:
         print("fun hours")
 
