9fc2bf8a6106d753eb27175cc3feaeb2451e6056
Tiago Pestana <tnevespestana@gmail.com>
2017-10-17 17:08:51 +0100

    generate multiple pages

--- app-4/script.py
+++ app-4/script.py
@@ -1,11 +1,17 @@
+# import Flask class object from flasj library
 from flask import Flask
 
+# instanciate the flask object
 app=Flask(__name__)
 
+# app.route defines the URL of the page relative to localhost
 @app.route('/')
-
 def home():
-    return "website content goes here"
+    return "home goes here"
+
+@app.route('/about/')
+def about():
+    return "about page goes here"
 
 if __name__=="__main__":
     app.run(debug=True)
