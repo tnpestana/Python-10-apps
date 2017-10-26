6c8f91dccfed4536aca518608b4a4c8d26d6f419
Tiago Pestana <tnevespestana@gmail.com>
2017-10-26 18:28:25 +0100

    simple km to miles converter

--- classes/14-section/script.py
+++ classes/14-section/script.py
@@ -5,15 +5,18 @@ from tkinter import *
 window=Tk()
 
 def km_to_miles():
-    print("x miles")
+    print(e1_value.get())
+    miles=float(e1_value.get())*1.6
+    t1.insert(END,miles)
 
 # create a new button
-b1=Button(window,text="Execute",command=km_to_miles())
+b1=Button(window,text="Convert",command=km_to_miles)
 # postiion the created button
 b1.grid(row=0,column=0)
 
 # create a new input area
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 
 # create a new text widget
