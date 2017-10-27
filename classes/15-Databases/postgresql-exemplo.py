d261a952b428ed69c039a604895c782506446e91
Tiago Pestana <tnevespestana@gmail.com>
2017-10-27 16:11:21 +0100

    delete data from postgresql table

--- classes/15-Databases/postgresql-exemplo.py
+++ classes/15-Databases/postgresql-exemplo.py
@@ -31,17 +31,18 @@ def insert(item,quantity,price):
 def delete(item):
     conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?",(item,))
+    cur.execute("DELETE FROM store WHERE item=%s",(item,))
     conn.commit()
     conn.close
 
 def update(item,quantity,price):
     conn=psycopg2.connect("dbname='database1' user='postgres' password='postgres123' host='localhost' port='5432'")
     cur=conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+    cur.execute("UPDATE store SET quantity=%s, price=%s WHERE item=%s",(quantity,price,item))
     conn.commit()
     conn.close
 
 create_table()
 #insert("Orange",10,2)
+delete("Orange")
 print(view())
